2a3feddc0862f2b630c6d734c92a7f13d05209cc
Newsstate24 <newsstate6294@gmail.com>
2025-03-21 11:07:01 +0530

    Fix Added default next.config.js and fixed routing issues

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,  // Enables React's strict mode
-  swcMinify: true,        // Enables SWC compiler for faster builds
-  images: {
-    domains: ["newsstate24.com"], // Allow images from your domain
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
