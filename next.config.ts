b016dc1a1e6129ed20a3927ca88f0f32a10f82ac
Newsstate24 <newsstate6294@gmail.com>
2025-03-21 10:57:47 +0530

    Fix Added default next.config.js and fixed routing issues

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true, // Enables React's strict mode for better error detection during development
-  swcMinify: true, // Enables SWC compiler for minifying JavaScript
-  images: {
-    domains: [], // Define allowed image domains for optimization
-  },
-  // You can add other Next.js configurations here, such as redirects, rewrites, etc.
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
